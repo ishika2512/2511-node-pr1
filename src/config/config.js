956974990a159193f1d2b2ac2960469edc0f4c05
ishika2512 <eshikaparmar2001@gmail.com>
2023-09-22 09:38:51 +0530

    add file

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,13 @@ dotenv.config();
 const envVarsSchema = Joi.object({
     PORT: Joi.number().default(7070),
     MONGODB_URL: Joi.string().trim().description("Mongodb url"),
+    SMTP_HOST: Joi.string().description("server that will send the emails"),
+    SMTP_PORT: Joi.number().description("port to connect to the email server"),
+    SMTP_USERNAME: Joi.string().description("username for email server"),
+    SMTP_PASSWORD: Joi.string().description("password for email server"),
+    EMAIL_FROM: Joi.string().description(
+        "the from field in the email sent by the app"
+    ),
 }).unknown();;
 
 const { value: envVars, error } = envVarsSchema
@@ -23,5 +30,16 @@ module.exports = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         },
+    },
+    email: {
+        smtp: {
+            host: envVars.SMTP_HOST,
+            port: envVars.SMTP_PORT,
+            auth: {
+                user: envVars.SMTP_USERNAME,
+                pass: envVars.SMTP_PASSWORD,
+            },
+        },
+        from: envVars.EMAIL_FROM,
     },
 };
